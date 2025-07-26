0b5530e88e5a8d929f5b9043500fe7ed4235d28d
Abhishek Shrestha <meabhi224@gmail.com>
2025-07-26 19:33:33 +0545

    updated sql ddl

--- sql/create-booking-table.sql
+++ sql/create-booking-table.sql
@@ -1,13 +1,14 @@
-CREATE TABLE booking (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    service INT NOT NULL,
-    consumer INT NOT NULL,
-    booking_date DATE NOT NULL,
-    -- Only stores selected date
-    status VARCHAR(50) NOT NULL DEFAULT 'pending',
-    -- Booking status
-    message TEXT,
-    -- Optional message from user
-    FOREIGN KEY (service) REFERENCES service(id),
-    FOREIGN KEY (consumer) REFERENCES users(id)
-);
+-- merosewa.booking definition
+CREATE TABLE `booking` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `service` int NOT NULL,
+    `status` varchar(50) NOT NULL,
+    `message` text,
+    `consumer` int NOT NULL,
+    `booking_date` date NOT NULL,
+    PRIMARY KEY (`id`),
+    KEY `service` (`service`),
+    KEY `consumer` (`consumer`),
+    CONSTRAINT `booking_ibfk_1` FOREIGN KEY (`service`) REFERENCES `service` (`id`),
+    CONSTRAINT `booking_ibfk_2` FOREIGN KEY (`consumer`) REFERENCES `users` (`id`)
+) ENGINE = InnoDB AUTO_INCREMENT = 10 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
--- sql/create-service-table.sql
+++ sql/create-service-table.sql
@@ -1,9 +1,12 @@
-CREATE TABLE service (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    service_provider INT NOT NULL,
-    name VARCHAR(100) NOT NULL,
-    description TEXT,
-    rate_per_hour FLOAT NOT NULL,
-    image VARCHAR(255) NOT NULL DEFAULT 'uploads/service/default.jpg',
-    FOREIGN KEY (service_provider) REFERENCES users(id)
-);
+-- merosewa.service definition
+CREATE TABLE `service` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `service_provider` int NOT NULL,
+    `name` varchar(100) NOT NULL,
+    `description` text,
+    `rate_per_hour` float NOT NULL,
+    `image` varchar(255) NOT NULL DEFAULT 'uploads/service/default.jpg',
+    PRIMARY KEY (`id`),
+    KEY `service_provider` (`service_provider`),
+    CONSTRAINT `service_ibfk_1` FOREIGN KEY (`service_provider`) REFERENCES `users` (`id`)
+) ENGINE = InnoDB AUTO_INCREMENT = 16 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
